ca966dd82b12ff188abc4ad54d2230c4860dfe71
Xewar <xewardreams@gmail.com>
2022-10-10 18:11:26 -0400

    card container formatting

--- src/CardContainer.js
+++ src/CardContainer.js
@@ -4,45 +4,30 @@ import uniqid from 'uniqid';
 import birdData from './birdData.js';
 
 const CardContainer = props => {
-  const { birdType, size, score } = props;
-  const [randomBirdState, setRandomBirdState] = useState([]);
-
-  useEffect(() => {
-    changeBirds();
-  }, [score]);
-
-  let birdsArray = birdData.birds;
-  let changeBirds = () => {
-    let randomBirds = [];
-    let randomBirdGenerator = () =>
-      Math.floor(Math.random() * birdsArray.length);
-    while (randomBirds.length < 9) {
-      let newBirdId = randomBirdGenerator();
-      if (randomBirds.indexOf(newBirdId) == -1) randomBirds.push(newBirdId);
-    }
-    setRandomBirdState(prevState => randomBirds);
-  };
-
-  const displayCards = () => {
-    return birdsArray.map(bird => {
-      if (randomBirdState.includes(bird.id)) {
-        return (
-          <Card
-            key={uniqid()}
-            url={`./images/birdsOfCP/${bird.fileExt}1.jpeg`}
-            birdType={bird.species}
-            updateScore={props.updateScore}
-          />
-        );
-      }
-    });
-  };
-
+  const displayCurrentBird = () => {};
+  const displayPrevBird = () => {};
   return (
     <div className="cardContainer">
-      <div className="leftCardStack"></div>
-      <div className="currentCards"></div>
-      <div className="rightCardStack"></div>
+      <div className="leftCardStack">
+        <div className="stackedCards">
+          <div className="cardBack card bottom">
+            <div class="cardText">Birds of NY</div>
+          </div>
+          <div className="cardBack card middle">
+            <div class="cardText">Birds of NY</div>
+          </div>
+          <div className="cardBack card top">
+            <div class="cardText">Birds of NY</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="currentCard card front">
+        <div class="birdPhoto">{displayCurrentBird}</div>
+      </div>
+      <div className="rightCardStack card">
+        <div class="smallBirdPhoto">{displayPrevBird}</div>
+      </div>
     </div>
   );
 };
