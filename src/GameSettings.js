5671f71ddee41116b45972b2573c104c2be6e240
Xewar <xewardreams@gmail.com>
2023-01-18 15:35:54 -0500

    added comment

--- src/GameSettings.js
+++ src/GameSettings.js
@@ -9,7 +9,7 @@ const GameSettings = props => {
     toggleDifficulty,
     toggleMode,
   } = props;
-
+  // returns the settings divs on the left side of the page (for difficulty, type of bird, etc.)
   return (
     <div className="settings">
       <div className="settingsGroup">
@@ -32,7 +32,7 @@ const GameSettings = props => {
         </div>
         <div className="select" id="herons">
           <button
-            className={'radioButton ' + (selectedBirds.herons && 'selected')}
+            className={'radioButton ' + (selectedBirds.herons & 'selected')}
             onClick={selectBirds}
           ></button>
           Bitterns, Herons, and Ibises
